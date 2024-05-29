b1ad5a3110ee02e6556c3c0f02d04a0e0af37410
Atomic <markus.schmidt42@online.de>
2024-05-29 17:58:52 +0200

    add steering comands

--- src/turtlebot_controller/turtlebot_controller/object_avoidance_v2.py
+++ src/turtlebot_controller/turtlebot_controller/object_avoidance_v2.py
@@ -7,25 +7,43 @@ import math
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from enum import Enum
+from geometry_msgs.msg import Twist
+
+
+class Behavior(Enum):
+    REORIENT = 1
+    AVOID_OBSTACLES = 2
+    TARGET_LOCK = 3
+
 
 class ObjectAvoidance(Node):
 
     def __init__(self):
         super().__init__("oav2_node")
         self.create_subscription(Image, "/image_raw", self.local_path_planning, 10)
+        self.movement_pub = self.create_publisher(Twist, '/cmd_vel', 10)
         self.cv_bridge = CvBridge()
         self.save_img_path = "/home/ubuntu/ros2_ws/images/modified/"
         self.counter = 0
+        self.behavior = Behavior.AVOID_OBSTACLES
         self.get_logger().info("ObjectAvoidance created successfully!")
 
     def local_path_planning(self, img:Image):
         self.get_logger().info("Starting path planning...")
         pi_cam_img = self.cv_bridge.imgmsg_to_cv2(img)
-        combined_obstacel_color_mask, red_color_mask = self.image_pre_processing(pi_cam_img)
-        hue_left, hue_middle, hue_right = self.mask_image(combined_obstacel_color_mask)
-        self.get_logger().info("hue_left: " + str(hue_left) +" | hue_middle: " + str(hue_middle) + " | hue_left: " + str(hue_right))
-        steer = self.calculate_movement(hue_left=hue_left, hue_middle=hue_middle, hue_right=hue_right)
-        self.get_logger().info("Steering move: " + steer)
+        obstacle_img, target_img = self.image_pre_processing(pi_cam_img)
+        self.swich_behavior(obstacle_img,target_img)
+
+        if self.behavior is Behavior.AVOID_OBSTACLES:
+            hue_left, hue_middle, hue_right = self.mask_image(obstacle_img)
+            self.calculate_movement(hue_left=hue_left, hue_middle=hue_middle, hue_right=hue_right)
+        elif self.behavior is Behavior.TARGET_LOCK:
+            hue_left, hue_middle, hue_right = self.mask_image(target_img)
+            self.calculate_movement(hue_left=hue_left, hue_middle=hue_middle, hue_right=hue_right)
+        else:
+            #turn 90° and reassess
+            self.ninety_degree_turn()
 
     def image_pre_processing(self, img):
         self.get_logger().info("Pre processing image...")
@@ -64,10 +82,10 @@ class ObjectAvoidance(Node):
         return combined_obstacel_color_mask, red_color_mask
     
     def mask_image(self, obstacel_img):
-
         # Calculate the width of each sliver
         width = obstacel_img.shape[1] // 3
 
+        # Seperate image into three segments
         img_mask_right = obstacel_img[:, :width]
         img_mask_middle = obstacel_img[:, width:2*width]
         img_mask_left = obstacel_img[:, 2*width:]
@@ -77,6 +95,7 @@ class ObjectAvoidance(Node):
         cv2.imwrite(self.save_img_path+"img_mask_middle"+str(self.counter)+".jpg", img_mask_middle)
         cv2.imwrite(self.save_img_path+"img_mask_left"+str(self.counter)+".jpg", img_mask_left)
 
+        # Calculate intensity of obstacles
         hue_right = np.mean(img_mask_right)
         hue_middle = np.mean(img_mask_middle)
         hue_left = np.mean(img_mask_left)
@@ -93,16 +112,26 @@ class ObjectAvoidance(Node):
         else:
             steer = "steer right"
         
+        self.get_logger().info("Steering move: " + steer)
         return steer
 
     def swich_behavior(self, obstacle_img, target_img):
-        behavior = ""
+        img_max_fill = 255
+        target_img_intensity = np.mean(target_img)
+        obstacle_img_intensity = np.mean(obstacle_img)
 
-        img_height, img_width, img_channels = obstacle_img.shape
-        img_max_fill = img_height * img_width * 255
+        if target_img_intensity > img_max_fill*(1/9):
+            self.behavior = Behavior.TARGET_LOCK
+        elif obstacle_img_intensity > img_max_fill*(4/5):
+            self.behavior = Behavior.REORIENT
+        else:
+            self.behavior = Behavior.AVOID_OBSTACLES
 
-
-
+    def ninety_degree_turn(self):
+        twist_msg = Twist()
+        twist_msg.angular.z = 1.57  # 90 degrees in radians
+        self.movement_pub.publish(twist_msg)
+        
 
 
 
