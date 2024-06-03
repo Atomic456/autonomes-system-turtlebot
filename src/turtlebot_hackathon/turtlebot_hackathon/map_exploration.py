93763e6b041b6f6ef53b6f9e78cf1b6609c87c16
Atomic <markus.schmidt42@online.de>
2024-06-03 08:53:00 +0200

    Map exploration

--- src/turtlebot_hackathon/turtlebot_hackathon/map_exploration.py
+++ src/turtlebot_hackathon/turtlebot_hackathon/map_exploration.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 import rclpy
+import random
 
 from rclpy.node import Node
+from std_msgs.msg import PoseStamped 
+from std_msgs.msg import OccupancyGrid
 
 
 class MapExploration(Node):
-
     def __init__(self):
         super().__init__('explor_note')
+        self.goal_pos_pub = self.create_publisher(PoseStamped, '/goal_pos', 10)
+        self.create_subscription(OccupancyGrid, '/map', self.safe_map,10)
+        
+        self.create_timer(10,self.calculate_goal_pos)
+
+    
+    def calculate_goal_pos(self):
+            goal_pos = PoseStamped()
+            goal_pos.header.frame_id = 'map'  # Set the reference frame (e.g., 'map')
+            goal_pos.pose.position.x = random.uniform(5.0, 5.9)  # Set the X-coordinate of the goal position
+            goal_pos.pose.position.y = random.uniform(0.5, 2.5)
+            self.goal_pos_pub.publish(goal_pos)
+
 
 
 def main(args=None):
